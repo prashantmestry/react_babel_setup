5cdee6a3926884965c76c4c8421e3814f0313898
prashant <prashant.mestry@dspim.com>
2020-09-13 13:38:21 +0530

    new modules added

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import reactDOM from 'react-dom';
+import './styles/style.scss';
+
+const App = () => {
+    return (<h1>App Entry Point</h1>) 
+}
+
+reactDOM.render(<App />, document.getElementById('root'));
+
+
